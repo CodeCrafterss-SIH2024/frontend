c6a719836c3240c3b879c18125264f814259e0c4
dvnshi <devanshijain460@gmail.com>
2024-09-08 14:58:17 +0530

    main page done

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import NewHeader from '../components/NewHeader';
+import Parliament from '../img/parliament-pic.png';
+import Boy from '../img/boy-pic.png';
+import './MainPage.css';
 
 const MainPage = () => {
   return (
     <div>
       <NewHeader/>
-      <h1 className='mt-[100px]'>this is my main page</h1>
+      {/* <h1 className='mt-[100px]'>this is my main page</h1> */}
+      <div className='body'>
+        <img src={Parliament} className='parliament'/>
+        <img src={Boy} className='boy1'/>
+        <img src={Boy} className='boy2'/>
+        <img src={Boy} className='boy3'/>  
+      </div>
+      
     </div>
   )
 }
